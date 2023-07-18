99ac1e110e2cffed761db189e2dae99f25112eb0
FIRMINOenzo <enzofirmino.camp@gmail.com>
2023-07-18 18:21:57 -0300

    feat: added post card

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,9 +1,14 @@
-import PostCard from './components/Home/PostCard'
+import DateCard from "./components/Home/DateCard";
+import PostCard from "./components/Home/PostCard";
 
 export default function Home() {
   return (
-    <main className="mt-4">
-      <PostCard/>
+    <main className="flex flex-col">
+      <DateCard date={"Julho, 2023"} />
+      <PostCard
+        title="000 - “Hello World!” - First post, what to think about this blog."
+        hashtags={["#oi", "#first_post", "#hello world"]}
+      />
     </main>
-  )
+  );
 }
