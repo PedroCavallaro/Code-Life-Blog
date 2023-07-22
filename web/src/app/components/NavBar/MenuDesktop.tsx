c9d56ea1b9b620823214123aec036b2b53a6073b
FIRMINOenzo <enzofirmino.camp@gmail.com>
2023-07-22 15:08:07 -0300

    chor: new folder organization

--- web/src/app/components/NavBar/MenuDesktop.tsx
+++ web/src/app/components/NavBar/MenuDesktop.tsx
@@ -1,48 +1,48 @@
-import page from "@/app/page";
+import page from "@/app/(main)/page";
 import Link from "next/link";
 
 enum Pages {
-    "CODE LIFE",
-    "About Us",
-    "Contact",
+  "CODE LIFE",
+  "About Us",
+  "Contact",
 }
 export default function MenuDesktop({
-    handlePageChange,
-    page,
+  handlePageChange,
+  page,
 }: {
-    handlePageChange: Function;
-    page: number;
+  handlePageChange: Function;
+  page: number;
 }) {
-    function handler(pageId: number) {
-        handlePageChange(pageId);
-    }
-    return (
-        <div className="gap-10 font-sans font-bold text-purple-50 hidden lg:flex">
-            <div className="flex flex-col">
-                <Link
-                    onClick={() => handler(1)}
-                    href={"/AboutUs"}
-                    className="hidden lg:flex "
-                >
-                    Sobre Nós
-                </Link>
-                {page === Pages["About Us"] && (
-                    <span className="w-auto h-1 bg-purple-800"></span>
-                )}
-            </div>
-            <div className="flex flex-col ">
-                <Link
-                    onClick={() => handler(2)}
-                    href={"/AboutUs"}
-                    className="hidden lg:flex
+  function handler(pageId: number) {
+    handlePageChange(pageId);
+  }
+  return (
+    <div className="gap-10 font-sans font-bold text-purple-50 hidden lg:flex">
+      <div className="flex flex-col">
+        <Link
+          onClick={() => handler(1)}
+          href={"/AboutUs"}
+          className="hidden lg:flex "
+        >
+          Sobre Nós
+        </Link>
+        {page === Pages["About Us"] && (
+          <span className="w-auto h-1 bg-purple-800"></span>
+        )}
+      </div>
+      <div className="flex flex-col ">
+        <Link
+          onClick={() => handler(2)}
+          href={"/AboutUs"}
+          className="hidden lg:flex
                          "
-                >
-                    Contatos
-                </Link>
-                {page === Pages["Contact"] && (
-                    <span className="w-auto h-1 bg-purple-800"></span>
-                )}
-            </div>
-        </div>
-    );
+        >
+          Contatos
+        </Link>
+        {page === Pages["Contact"] && (
+          <span className="w-auto h-1 bg-purple-800"></span>
+        )}
+      </div>
+    </div>
+  );
 }
